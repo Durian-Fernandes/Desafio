dd6bfb04a76aa6eed3d65c8b4a5877077cf107d8
Durian Fernandes <durianfernandesr7@gmail.com>
2023-09-01 16:55:24 -0300

    Atualização git commit -a -m Atualização

--- Desafio.js
+++ Desafio.js
@@ -3,17 +3,17 @@ function calcularConta() {
   const quantidadeAntes = parseInt(document.getElementById("quantidadeAntes").value);
   const valorAposHappyHour = parseFloat(document.getElementById("aposHappyHour").value);
   const quantidadeApos = parseInt(document.getElementById("quantidadeApos").value);
-  const porcentagem = parseFloat(document.getElementById("porcentagem").value);
+ 
 
   const totalAntes = valorAntesHappyHour * quantidadeAntes;
   const totalApos = valorAposHappyHour * quantidadeApos;
   const subtotal = totalAntes + totalApos;
-  const taxaServico = (porcentagem / 100) * subtotal;
+  const taxaServico = 0.10 * subtotal;
   const valorTotal = subtotal + taxaServico;
 
   const resultado = `
       <p>Subtotal: R$ ${subtotal.toFixed(2)}</p>
-      <p>Taxa de Serviço (${porcentagem}%): R$ ${taxaServico.toFixed(2)}</p>
+      <p>Taxa de Serviço 10%: R$ ${taxaServico.toFixed(2)}</p>
       <p>Valor Total: R$ ${valorTotal.toFixed(2)}</p>
   `;
 
